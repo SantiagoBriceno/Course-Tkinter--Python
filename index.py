66f9fb59aaf563400b6af8f716f4b6622fa4653d
Santiago <Santiagogbf2000@Gmail.com>
2022-10-26 19:12:21 -0400

    CRUD parcialmente agregado

--- index.py
+++ index.py
@@ -5,6 +5,8 @@ import sqlite3
 
 class Product:
 
+    dbname = 'database.db'
+
     def __init__(self, window):
         self.wind = window
         self.wind.title("Products Aplication")
@@ -26,7 +28,11 @@ class Product:
         self.price.grid(row = 2, column = 1)
 
         # Button add Product 
-        ttk.Button(frame, text = 'Save Product').grid(row = 3, columnspan = 2, sticky = W+E)
+        ttk.Button(frame, text = 'Save Product', command = self.add_product).grid(row = 3, columnspan = 2, sticky = W+E)
+
+        # Mensajes de aviso
+        self.message = Label(text = '', fg = 'red')
+        self.message.grid(row = 3, column = 0, columnspan=2, sticky = W + E)
 
         #Table
         self.tree = ttk.Treeview(height = 10, columns = 2)
@@ -34,6 +40,52 @@ class Product:
         self.tree.heading("#0", text='Name', anchor = CENTER)
         self.tree.heading("#1", text='Price', anchor = CENTER)
 
+        self.get_products()
+
+
+    def run_query(self, query, parameters = ()):
+        with sqlite3.connect(self.dbname) as conn:
+            cursor = conn.cursor()
+            result = cursor.execute(query, parameters)
+            conn.commit()
+        return result
+
+    def get_products(self):
+
+        #Cleaning table
+        records = self.tree.get_children()
+        for element in records:
+            self.tree.delete(element)
+
+        #quering data
+        query = 'SELECT * FROM product ORDER BY name DESC'
+        db_rows = self.run_query(query)
+        for row in db_rows:
+            self.tree.insert('', 0, text = row[1], values = row[2])
+
+    def validation(self):
+        return len(self.name.get()) != 0 and len(self.price.get()) != 0
+
+    def add_product(self):
+        if self.validation():
+            print(self.name.get())
+            print(self.price.get())
+
+            query = 'INSERT INTO product VALUES(NULL, ?, ?)'
+            productName = self.name.get()
+            productPrice = self.price.get()
+            parameters = (productName, productPrice)
+            self.run_query(query, parameters)
+            self.message['text'] = 'El producto {} ha sido agregado'.format(productName)
+
+            #Borramos los campos de textos
+            self.name.delete(0, END)
+            self.price.delete(0, END)
+
+        else:
+            self.message['text'] = 'El nombre y el precio son requeridos'
+        self.get_products()
+        
 
 
 if __name__ == '__main__':
